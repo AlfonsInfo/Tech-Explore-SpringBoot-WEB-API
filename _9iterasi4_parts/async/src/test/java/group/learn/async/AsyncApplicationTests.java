be88e5203c1769a2e75035b4492897af90dd2af7
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2024-01-11 10:41:58 +0700

    init spring async

---
+++ _9iterasi4_parts/async/src/test/java/group/learn/async/AsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package group.learn.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
