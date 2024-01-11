fc58e003653c0873f610c3f9001a316372c7ab76
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2024-01-11 10:32:32 +0700

    init project for jpa - webmvc

---
+++ _9iterasi4_parts/restful-api/src/main/java/group/learn/restfulapi/RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package group.learn.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
---
+++ _9iterasi4_parts/restful-api/src/test/java/group/learn/restfulapi/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package group.learn.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
